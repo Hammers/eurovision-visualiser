0f27349ef9883940b98fa173d4a5e6067117eb4f
Ryan Ayton <ryanianhamlet@gmail.com>
2020-02-03 21:59:14 +0000

    Initial commit

---
+++ client/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="small">
+    {{value}}
+  </div>
+</template>
+
+<script>
+  import io from "socket.io-client";
+  var socket = io.connect("http://localhost:3000");
+
+  export default {
+    data() {
+      return {
+        value: 0
+      };
+    },
+    created() {
+      this.getRealtimeData()
+    },
+    methods: {
+      getRealtimeData() {
+        socket.on("newdata", fetchedData => {
+          console.log("data received");
+          this.value = fetchedData;
+        })
+      },
+    }
+  };
+</script>
+
+<style>
+
+</style>
---
+++ server/index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+const server = app.listen(`${port}`, function() {
+    console.log(`Server started on port ${port}`);
+});
+
+const io = require("socket.io")(server);
+
+
+
+function getRandomValue(){
+    return Math.floor(Math.random() * (50 - 5 + 1)) + 5;
+}
+
+io.on("connection", socket => {
+    console.log("User connected");
+    setInterval(() => {
+        socket.broadcast.emit("newdata", getRandomValue())
+    }, 5000)
+});
